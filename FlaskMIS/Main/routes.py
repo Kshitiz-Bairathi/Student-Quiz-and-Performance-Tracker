98f0750812a2a3f9c39df534fc695a21eb2c36e2
Kshitiz-Bairathi <bairathikshitiz@gmail.com>
2025-09-06 00:10:36 +0530

    Update routes.py

--- FlaskMIS/Main/routes.py
+++ FlaskMIS/Main/routes.py
@@ -5,20 +5,6 @@ from Python_Part.Admin_class import Admin_class
 from Python_Part.Student_class import Student_class
 
 Main = Blueprint('Main', __name__)
-from FlaskMIS import db, bcrypt
-from FlaskMIS.models import Admins
-@Main.route("/init-db")
-def init_db():
-    db.create_all()
-    return "✅ Tables created in PostgreSQL"
-
-@Main.route("/create-admin")
-def create_admin():
-    pw = bcrypt.generate_password_hash("Bairathi").decode("utf-8")
-    admin = Admins(username="Kshitiz", password=pw)
-    db.session.add(admin)
-    db.session.commit()
-    return "✅ Admin created: username=admin, password=admin123"
 
 @Main.route("/")
 @Main.route("/home")
